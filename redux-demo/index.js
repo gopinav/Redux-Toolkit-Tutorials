37a6fafe003e41ac7104b003e40c153fa4c10272
Vishwas <vishwas.g5@gmail.com>
2022-02-13 22:49:46 +0530

    Store, actions and reducers

---
+++ redux-demo/index.js
@@ -0,0 +1,35 @@
+const redux = require('redux')
+const createStore = redux.createStore
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  }
+}
+
+const initialState = {
+  numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+    default:
+      return state
+  }
+}
+const store = createStore(reducer)
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() =>
+  console.log('Updated State ', store.getState())
+)
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
