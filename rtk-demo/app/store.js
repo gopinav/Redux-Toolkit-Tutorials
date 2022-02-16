df4a6973643a0eec3cf275dc9d551ecb471a415f
Vishwas <vishwas.g5@gmail.com>
2022-02-16 19:27:15 +0530

    Add logger middleware

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,15 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer
-  }
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
