584def0da7cbf4588e3e7173a3a7af8491d50f73
Vishwas <vishwas.g5@gmail.com>
2022-02-18 06:27:32 +0530

    useDispatch hook

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './cakeSlice'
 
 export const CakeView = () => {
-  const cake = useSelector(state => state.cake)
+  const numOfCakes = useSelector(state => state.cake.numOfCakes)
+  const dispatch = useDispatch()
   return (
     <div>
-      <h2>Number of cakes - {cake.numOfCakes}</h2>
-      <button>Order Cake</button>
-      <button>Restock Cakes</button>
+      <h2>Number of cakes - {numOfCakes}</h2>
+      <button onClick={() => dispatch(ordered())}>Order Cake</button>
+      <button onClick={() => dispatch(restocked(5))}>Restock Cakes</button>
     </div>
   )
 }
--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
+
 export const IcecreamView = () => {
-  const icecream = useSelector(state => state.icecream)
+  const [value, setValue] = React.useState(1)
+  const numOfIcecreams = useSelector(state => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
   return (
     <div>
-      <h2>Number of ice creams - {icecream.numOfIcecreams}</h2>
-      <button>Order Ice cream</button>
-      <button>Restock Ice creams</button>
+      <h2>Number of ice creams - {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+      <input
+        type='number'
+        value={value}
+        onChange={e => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock Ice creams
+      </button>
     </div>
   )
 }
